c9af427c4b26b7009eea01caaf99513ebf6b4fcb
ZamirZhiianbekov <zamirbekaripovich@gmail.com>
2023-06-08 19:50:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// alert("Hello Javascript")
+
+// const place = confirm("Are you here")
+// console.log(place)
+
+// const currentPlace = prompt("Are you here?", "Tashkent")
+
+// console.log(typeof(currentPlace))
+
+// const age = +prompt("How old are you", "18")
+// console.log(age + 10);
+
+// const favoriteColor = []
+
+
+// favoriteColor[0] = prompt("What your faworite color #1")
+// favoriteColor[1] = prompt("What your faworite color #2")
+// favoriteColor[2] = prompt("What your faworite color #3")
+
+// console.log(typeof favoriteColor )
+
+// const channel = prompt("What's your favorite youtuber")
+
+
+// console.log(`https://youtube.com/${channel}`)
+
+// const user = "Samar";
+// const job = "Javascript developer";
+
+// console.log(`User name is ${user}, he is ${job}`)
+
+// console.log(6 +  +"9")
+
+// incr decr
+
+// let incr =  10;
+// let decr = 10;
+
+
+// incr++
+// decr--
+
+// console.log(incr++)
+// console.log(decr--)
+
+// console.log(10 % 4)
+
+// console.log(5 * 5 === "25")
+
+// const isAge = true;
+
+// const isClose = false;
+
+// console.log(isAge && isClose)
+
+
